afc33edfcfc6459e5f27bbbc6296c262e0fbc1e8
Bdwrds <edwards.benjamin.james@gmail.com>
2022-04-22 13:19:18 +0100

    tab structure for #1

--- app.py
+++ app.py
@@ -1,44 +1,10 @@
+"""
+Structure influence by: https://www.purfe.com/dash-project-structure-multi-tab-app-with-callbacks-in-different-files/
+author: Ben E
+date: 2022-04-22
+"""
+from dashboard.content import app
 
-from dash import Dash, html, dcc
-
-app = Dash(__name__)
-
-app.layout = html.Div([
-    html.Div(children=[
-        html.Label('Dropdown'),
-        dcc.Dropdown(['New York City', 'Montréal', 'San Francisco'], 'Montréal'),
-
-        html.Br(),
-        html.Label('Multi-Select Dropdown'),
-        dcc.Dropdown(['New York City', 'Montréal', 'San Francisco'],
-                     ['Montréal', 'San Francisco'],
-                     multi=True),
-
-        html.Br(),
-        html.Label('Radio Items'),
-        dcc.RadioItems(['New York City', 'Montréal', 'San Francisco'], 'Montréal'),
-    ], style={'padding': 10, 'flex': 1}),
-
-    html.Div(children=[
-        html.Label('Checkboxes'),
-        dcc.Checklist(['New York City', 'Montréal', 'San Francisco'],
-                      ['Montréal', 'San Francisco']
-        ),
-
-        html.Br(),
-        html.Label('Text Input'),
-        dcc.Input(value='MTL', type='text'),
-
-        html.Br(),
-        html.Label('Slider'),
-        dcc.Slider(
-            min=0,
-            max=9,
-            marks={i: f'Label {i}' if i == 1 else str(i) for i in range(1, 6)},
-            value=5,
-        ),
-    ], style={'padding': 10, 'flex': 1})
-], style={'display': 'flex', 'flex-direction': 'row'})
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+server = app.server
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8050)
